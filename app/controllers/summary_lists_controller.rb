08f2e5b0e2e82a86d21474e4985b07b86291103a
Tetsuma Yoshino <usdeurjpy0305@gmail.com>
2013-08-14 01:13:50 -0700

    show summary list (but, under constract yet...)

--- app/controllers/summary_lists_controller.rb
+++ app/controllers/summary_lists_controller.rb
@@ -1,36 +1,41 @@
 class SummaryListsController < ApplicationController
-#changed at improving calcration system. but, calcration will be slowly...
-NUM_CALC_ARRAY = 10
-
 	def index
 		@article = Article.find_by id: params[:articleId]
 		
                 #create array for calcration 
                 arrayCount = 0
-                Summary.where(:article_id =>@article.id).find_each do |summary|  
+		scoreList = Array.new 
+
+                Summary.where(:article_id => @article.id).find_each do |summary|  
 		
 			#calcurate sum of the goodSummaryPoint from summary_id.
-			goodSummaryPoint = 0
+			goodSummaryPoint = GoodSummary.where(:summary_id => summary.id).count
 			
-			GoodSummary.where(:summary_id => summary.id.find_each do
-				goodSummaryPoint++
-			end
-		
-			#calcu
+			#scoreItem
+			scoreItem = Array.new([summary.id,goodSummaryPoint])
+			scoreList[arrayCount] = scoreItem
 
-	
-			#if array is already max, break
-			if arrayCount >= NUM_CALC_ARRAY then
-				break
-			end 
+			#inc arrayCount
+			arrayCount = arrayCount + 1
+
 		end 
-                
+               
+
+		#sort summary list
+		#
+		#
+		#Under Construction
+		#
+		#
+
+		#insert to @summarys 
+		@summarys = Array.new
+		@sumUsers = Array.new
+		scoreList.each_with_index do |scoreItem, i| 				
+			@summarys[i] = Summary.find_by id: scoreItem[0]
+			@sumUsers[i] = User.find_by id: @summarys[i].user_id  
+		end
 		
-		
-		#if(@summary != nil)
-	        #        @article  = Article.find_by id: @summary.article_id
-		#	@sumUser =  User.find_by id: @summary.user_id
-		#end
 	end
        
         def show
