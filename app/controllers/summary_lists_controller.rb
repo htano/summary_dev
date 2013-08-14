6c5e2728e7facf8cdd79ba63e3615b5adbfab1a8
Tetsuma Yoshino <usdeurjpy0305@gmail.com>
2013-08-14 06:14:33 -0700

    ii youyaku botan developed

--- app/controllers/summary_lists_controller.rb
+++ app/controllers/summary_lists_controller.rb
@@ -1,16 +1,24 @@
 class SummaryListsController < ApplicationController
 	def index
 		@article = Article.find_by id: params[:articleId]
-		
-                #create array for calcration 
-                arrayCount = 0
-		scoreList = Array.new 
 
-                Summary.where(:article_id => @article.id).find_each do |summary|  
+		#create array for calcration 
+		arrayCount = 0
+		scoreList = Array.new 
 		
+		@isGoodCompleted = Array.new
+
+		Summary.where(:article_id => @article.id).find_each do |summary|  
+
 			#calcurate sum of the goodSummaryPoint from summary_id.
-			goodSummaryPoint = GoodSummary.where(:summary_id => summary.id).count
-			
+			goodSummary = GoodSummary.where(:summary_id => summary.id)
+			goodSummaryPoint = goodSummary.count
+			unless goodSummary.find_by(:user_id => getLoginUser.id) == nil then 
+				@isGoodCompleted[arrayCount] = true
+			else
+				@isGoodCompleted[arrayCount] = false 
+			end	
+
 			#scoreItem
 			scoreItem = Array.new([summary.id,goodSummaryPoint])
 			scoreList[arrayCount] = scoreItem
@@ -19,7 +27,7 @@ class SummaryListsController < ApplicationController
 			arrayCount = arrayCount + 1
 
 		end 
-               
+
 
 		#sort summary list
 		#
@@ -28,17 +36,26 @@ class SummaryListsController < ApplicationController
 		#
 		#
 
-		#insert to @summarys 
+		#insert to each params  
 		@summarys = Array.new
 		@sumUsers = Array.new
+		@goodSummaryPoint = Array.new
 		scoreList.each_with_index do |scoreItem, i| 				
 			@summarys[i] = Summary.find_by id: scoreItem[0]
 			@sumUsers[i] = User.find_by id: @summarys[i].user_id  
+			@goodSummaryPoint[i] = scoreList[i][1] 
 		end
-		
-	end
-       
-        def show
 
-        end
+	end
+
+	def goodSummary 
+
+		goodSummary = GoodSummary.new(:user_id => getLoginUser.id, :summary_id =>params[:summaryId]) 
+
+		if goodSummary.save
+			#nothing yet
+			redirect_to :action => "index", :artcileId =>params[:articleId]
+		end
+
+	end
 end
