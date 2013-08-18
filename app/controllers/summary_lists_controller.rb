1932a9e39cd5afb325ee5d8f19e5212cb9771317
Tetsuma Yoshino <usdeurjpy0305@gmail.com>
2013-08-18 20:37:17 +0900

    refactoring some modules

--- app/controllers/summary_lists_controller.rb
+++ app/controllers/summary_lists_controller.rb
@@ -1,67 +1,20 @@
 class SummaryListsController < ApplicationController
 	def index
 		#check current loginuser
-			
 		@article = Article.find_by id: params[:articleId]
 		if @article == nil then
 			redirect_to :controller => "mypage", :action => "index" 
 			return
 		end
-
-		#create array for calcration 
-		arrayCount = 0
-		scoreList = Array.new 
-		
-		@isGoodCompleted = Array.new
-
-		Summary.where(:article_id => @article.id).find_each do |summary|  
-
-			#calcurate sum of the goodSummaryPoint from summary_id.
-			goodSummary = GoodSummary.where(:summary_id => summary.id)
-			goodSummaryPoint = goodSummary.count
-			unless getLoginUser == nil then
-				unless goodSummary.find_by(:user_id => getLoginUser.id) == nil then 
-					@isGoodCompleted[arrayCount] = true
-				else
-					@isGoodCompleted[arrayCount] = false 
-				end
-			else
-				@isGoodCompleted[arrayCount] = false 
-			end	
-
-			#scoreItem
-			scoreItem = Array.new([summary.id,goodSummaryPoint])
-			scoreList[arrayCount] = scoreItem
-
-			#inc arrayCount
-			arrayCount = arrayCount + 1
-
-		end 
-
-
-		#sort summary list
-		#
-		#
-		#Under Construction
-		#
-		#
-
-		#insert to each params  
-		@summarys = Array.new
-		@sumUsers = Array.new
-		@goodSummaryPoint = Array.new
-		scoreList.each_with_index do |scoreItem, i| 				
-			@summarys[i] = Summary.find_by id: scoreItem[0]
-			@sumUsers[i] = User.find_by id: @summarys[i].user_id  
-			@goodSummaryPoint[i] = scoreList[i][1] 
-		end
+		@user = getLoginUser
+		@summaryList = @article.getSortedSummaryList(@user, @article)
 
 	end
 
 	def goodSummary 
 		if getLoginUser == nil then
 			redirect_to :controller => "consumer", :action => "index"
-		    return	
+			return	
 		end
 		goodSummary = GoodSummary.new(:user_id => getLoginUser.id, :summary_id =>params[:summaryId]) 
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,47 @@
 class Article < ActiveRecord::Base
-  has_many :user_articles, :dependent => :destroy
-  has_many :summaries, :dependent => :destroy
+	has_many :user_articles, :dependent => :destroy
+	has_many :summaries, :dependent => :destroy
+
+	def getSortedSummaryList(user,article)
+		#create array for calcration 
+		scoreItem = Struct.new(:summary,:goodSummaryPoint)
+		scoreList = Array.new 
+		isGoodCompleted = Array.new
+
+		article.summaries.each_with_index do |summary,i|  
+
+			#calcurate goodSummaryPoint 
+			goodSummaryPoint = summary.good_summaries.count
+
+			#scoreItem
+			scoreList[i] = scoreItem.new(summary, goodSummaryPoint)
+		end 
+
+
+		#sort summary list
+		#
+		#
+		#Under Construction
+		#
+		#
+
+		summaryItem = Struct.new(:summary, :user, :summaryPoint, :isGoodCompleted) 
+		summaryList = Array.new
+		#insert to each params  
+		scoreList.each_with_index do |scoreItem, i| 				
+			unless user == nil then
+				unless scoreItem.summary.good_summaries.find_by(:user_id => user.id) == nil then 
+					isGoodCompleted = true
+				else
+					isGoodCompleted = false 
+				end
+			else
+				isGoodCompleted = false 
+			end	
+			summaryList[i] = summaryItem.new(scoreItem.summary, scoreItem.summary.user,scoreItem.goodSummaryPoint,isGoodCompleted)  
+		end
+
+		return summaryList
+	end
+
 end
