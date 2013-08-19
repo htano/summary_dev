f4f6add3c1d25bbfd8a230f6c5cc1ace3375a108
shingo0809 <shingo0809@gmail.com>
2013-08-19 23:59:08 +0900

    2013/08/19 23:59

--- app/controllers/summary_controller.rb
+++ app/controllers/summary_controller.rb
@@ -3,7 +3,12 @@
 class SummaryController < ApplicationController
   def edit
     if signed_in?
-      article = Article.find(params[:article_id]);
+      article = Article.find_by id: params[:articleId]
+      if article == nil then
+        redirect_to :controller => "mypage", :action => "index" 
+        return
+      end
+      #article = existArticle?(params[:article_id]);
       @url = article.url;
       @title = article.title;
       user_id = getLoginUser.id;
@@ -19,7 +24,12 @@ class SummaryController < ApplicationController
 
   def edit_confirm
     if signed_in?
-      article = Article.find(params[:article_id]);
+      article = Article.find_by id: params[:articleId]
+      if article == nil then
+        redirect_to :controller => "mypage", :action => "index" 
+        return
+      end
+      #article = existArticle?(params[:article_id]);
       @url = article.url;
       @title = article.title;
       user_id = getLoginUser.id;
@@ -33,6 +43,12 @@ class SummaryController < ApplicationController
   def edit_complete
     if signed_in?
       user_id = getLoginUser.id;
+      article = Article.find_by id: params[:articleId]
+      if article == nil then
+        redirect_to :controller => "mypage", :action => "index" 
+        return
+      end
+      #article = existArticle?(params[:article_id]);
       summary = Summary.find_by_user_id_and_article_id(user_id, params[:article_id]);
       if summary != nil then
         #すでに当該記事に対して要約が登録されていた場合、以下の処理をする
@@ -54,7 +70,12 @@ class SummaryController < ApplicationController
 
   def show
     if signed_in?
-      article = Article.find(params[:article_id]);
+      article = Article.find_by id: params[:articleId]
+      if article == nil then
+        redirect_to :controller => "mypage", :action => "index" 
+        return
+      end
+      #article = existArticle?(params[:article_id]);
       @url = article.url;
       @title = article.title;
       user_id = getLoginUser.id;
@@ -64,6 +85,19 @@ class SummaryController < ApplicationController
       @msg = "要約が登録出来ました！"
     else
       redirect_to :controller => "consumer", :action => "index";
+    end
+  end
+
+  def existArticle?(article_id)
+    @article = Article.returnArticle(article_id);
+    if @article == nil then
+    logger.error("!")
+    redirect_to :controller => "mypage", :action => "index";
+      #render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false, :content_type => 'text/html'
+      return
+    else
+    logger.error("!!")
+      return @article
     end
   end
 end
