0a1b080bbe942ca76b65e079362abce6e53f505f
toru1055 <toru1055h@gmail.com>
2013-08-23 21:44:21 +0900

    Update ChromeExt to add webpage/add function(#14).

--- chrome_ext/popup.js
+++ chrome_ext/popup.js
@@ -1,6 +1,25 @@
-$(document).ready(function(){
+$(document).ready( function(){
   var bg = window.chrome.extension.getBackgroundPage();
   console.log( $("p#p_title").text() );
-  $("p#p_title").text( bg.current_tab.title);
-  $("p#p_url").text( bg.current_tab.url);
+  $("p#p_title").text(bg.current_tab.title);
+  $("p#p_url").text(bg.current_tab.url);
+
+  $('.popup_btn').click(function(){
+    $("img.a_load").attr("style", "visibility:visible;");
+    $.ajax({
+      url: 'http://localhost:3000/webpage/add',
+      type: 'GET',
+      data: 'booked_url=' + escape(bg.current_tab.url),
+      success: function(data) {
+        $("img.a_load").attr("style", "visibility:hidden;");
+        $("p#p_comment").attr("style", "visibility:visible;");
+        $("p#p_comment").text("登録しました。");
+      },
+      error: function(data) {
+        $("img.a_load").attr("style", "visibility:hidden;");
+        $("p#p_comment").attr("style", "visibility:visible;");
+        $("p#p_comment").text("失敗しました、urlを確認して下さい。");
+      }
+    });
+  });
 });
