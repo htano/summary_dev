34f300b24d25a8779bccb30174f7c7bf6498049d
toru1055 <toru1055h@gmail.com>
2013-09-04 21:44:25 +0900

    Update image file path to correspond to production env(#28).

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ SummaryDev::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = false
@@ -89,5 +89,5 @@ SummaryDev::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # FIXME Temporary config
-  config.assets.debug = true
+  #config.assets.debug = true
 end
