8ede5f6066174c07bfed886ce9e54eb2466eb4d9
shingo0809 <shingo0809@gmail.com>
2013-09-25 21:56:11 +0900

    2013/09/25 21:56

--- app/models/user_article.rb
+++ app/models/user_article.rb
@@ -1,1 +1,5 @@
-class UserArticle < ActiveRecord::Base  belongs_to :user  belongs_to :article  has_many :user_article_tags, :dependent => :destroyend
+class UserArticle < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :article
+  has_many :user_article_tags, :dependent => :destroy
+end
