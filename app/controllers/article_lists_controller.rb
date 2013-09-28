5417b94d7c17113ddaef9e02c987f28e0b39b23b
shingo0809 <shingo0809@gmail.com>
2013-09-28 10:25:34 +0900

    2013/09/28 10:25 remove article_lists_controller

--- app/controllers/article_lists_controller.rb
+++
@@ -1,6 +0,0 @@
-class ArticleListsController < ApplicationController
-  def list_by_tag
-    @tag = "#{params[:tag]}"
-    @articles = Article.get_list_by_tag(@tag)
-  end
-end
--- test/helpers/article_lists_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ArticleListsHelperTest < ActionView::TestCase
-end
