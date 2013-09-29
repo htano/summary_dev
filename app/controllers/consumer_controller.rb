726f1535a683da3bcec5b9ba4ce897252dded99a
toru1055 <toru1055h@gmail.com>
2013-09-29 13:49:07 +0900

    Refactor session consumer controller.

--- app/controllers/consumer_controller.rb
+++ app/controllers/consumer_controller.rb
@@ -1,5 +1,4 @@
 require 'pathname'
-
 require "openid"
 require 'openid/extensions/sreg'
 require 'openid/extensions/pape'
@@ -9,10 +8,11 @@ class ConsumerController < ApplicationController
   layout nil
 
   def oauth_complete
-    #logger.debug env['omniauth.auth'].to_yaml
-    @oauth_url = "oauth://" + env['omniauth.auth']['provider'] + "/" + env['omniauth.auth']['uid']
+    @oauth_url = "oauth://" + env['omniauth.auth']['provider'] + 
+      "/" + env['omniauth.auth']['uid']
     @image_url = env['omniauth.auth'].info.image
     session[:openid_url] = @oauth_url
+    @redirect_url = url_for(:controller => 'mypage', :action => 'index')
     if getLoginUser
       @uname = getLoginUser.name
       flash[:success] = "Hello " + @uname + ". Login processing was successful."
@@ -20,28 +20,32 @@ class ConsumerController < ApplicationController
       if getLoginUser.update_last_login
         if env['omniauth.params']['keep_login'] == "on"
           getLoginUser.update_keep_login(@remote_ip)
-          cookies[:keep_login_token] = { :value => getLoginUser.keep_login_token, :expires => Time.now + 3.days }
+          cookies[:keep_login_token] = {
+            :value => getLoginUser.keep_login_token,
+            :expires => Time.now + 3.days
+          }
         end
       else
         #TODO Error handling
       end
       if env['omniauth.origin']
-        redirect_to env['omniauth.origin']
-      else
-        redirect_to :controller => 'mypage', :action => 'index'
+        @redirect_url = env['omniauth.origin']
       end
     else
       if env['omniauth.origin']
-        redirect_to :action => 'signup', :image => @image_url,:fromUrl => env['omniauth.origin']
+        @redirect_url = url_for(:action => 'signup',
+                                :image => @image_url,
+                                :fromUrl => env['omniauth.origin'])
       else
-        redirect_to :action => 'signup', :image => @image_url
+        @redirect_url = url_for(:action => 'signup', :image => @image_url)
       end
     end
+    redirect_to(@redirect_url)
   end
 
   def index
     if signed_in?
-      redirect_to :controller => 'mypage', :action => 'index'
+      redirect_to(:controller => 'mypage', :action => 'index')
     end
     # render an openid form
   end
@@ -51,50 +55,36 @@ class ConsumerController < ApplicationController
       identifier = params[:openid_identifier]
       if identifier.nil?
         flash[:error] = "Enter an OpenID identifier"
-        redirect_to :action => 'index'
+        redirect_to(:action => 'index')
         return
       end
       oidreq = consumer.begin(identifier)
     rescue OpenID::OpenIDError => e
       flash[:error] = "Discovery failed for #{identifier}: #{e}"
-      redirect_to :action => 'index'
+      redirect_to(:action => 'index')
       return
     end
-    if params[:use_sreg]
-      sregreq = OpenID::SReg::Request.new
-      # required fields
-      sregreq.request_fields(['email','nickname'], true)
-      # optional fields
-      sregreq.request_fields(['dob', 'fullname'], false)
-      oidreq.add_extension(sregreq)
-      oidreq.return_to_args['did_sreg'] = 'y'
-    end
-    if params[:use_pape]
-      papereq = OpenID::PAPE::Request.new
-      papereq.add_policy_uri(OpenID::PAPE::AUTH_PHISHING_RESISTANT)
-      papereq.max_auth_age = 2*60*60
-      oidreq.add_extension(papereq)
-      oidreq.return_to_args['did_pape'] = 'y'
-    end
-    if params[:force_post]
-      oidreq.return_to_args['force_post']='x'*2048
-    end
-    return_to = url_for :action => 'complete', :only_path => false, :fromUrl => params[:fromUrl], :keep_login => params[:keep_login]
+    return_to = url_for(:action => 'complete',
+                        :only_path => false,
+                        :fromUrl => params[:fromUrl],
+                        :keep_login => params[:keep_login])
     realm = url_for :action => '', :id => nil, :only_path => false
     
     if oidreq.send_redirect?(realm, return_to, params[:immediate])
-      redirect_to oidreq.redirect_url(realm, return_to, params[:immediate])
+      redirect_to(oidreq.redirect_url(realm, return_to, params[:immediate]))
     else
-      render :text => oidreq.html_markup(realm, return_to, params[:immediate], {'id' => 'openid_form'})
+      render(:text => oidreq.html_markup(realm,
+                                         return_to,
+                                         params[:immediate],
+                                         {'id' => 'openid_form'}))
     end
   end
 
   def complete
-    # FIXME - url_for some action is not necessarily the current URL.
     current_url = url_for(:action => 'complete', :only_path => false)
     parameters = params.reject{|k,v|request.path_parameters[k]}.reject{|k,v| k == 'action' || k == 'controller'}
-    #parameters = params.reject{|k,v|request.path_parameters[k]}
     oidresp = consumer.complete(parameters, current_url)
+    @redirect_url = url_for(:action => 'index')
     case oidresp.status
     when OpenID::Consumer::FAILURE
       if oidresp.display_identifier
@@ -103,46 +93,13 @@ class ConsumerController < ApplicationController
       else
         flash[:error] = "Verification failed: #{oidresp.message}"
       end
-    when OpenID::Consumer::SUCCESS
-      flash[:success] = ("Verification of #{oidresp.display_identifier}"\
-                         " succeeded.")
-      if params[:did_sreg]
-        sreg_resp = OpenID::SReg::Response.from_success_response(oidresp)
-        sreg_message = "Simple Registration data was requested"
-        if sreg_resp.empty?
-          sreg_message << ", but none was returned."
-        else
-          sreg_message << ". The following data were sent:"
-          sreg_resp.data.each {|k,v|
-            sreg_message << "<br/><b>#{k}</b>: #{v}"
-          }
-        end
-        flash[:sreg_results] = sreg_message
-      end
-      if params[:did_pape]
-        pape_resp = OpenID::PAPE::Response.from_success_response(oidresp)
-        pape_message = "A phishing resistant authentication method was requested"
-        if pape_resp.auth_policies.member? OpenID::PAPE::AUTH_PHISHING_RESISTANT
-          pape_message << ", and the server reported one."
-        else
-          pape_message << ", but the server did not report one."
-        end
-        if pape_resp.auth_time
-          pape_message << "<br><b>Authentication time:</b> #{pape_resp.auth_time} seconds"
-        end
-        if pape_resp.nist_auth_level
-          pape_message << "<br><b>NIST Auth Level:</b> #{pape_resp.nist_auth_level}"
-        end
-        flash[:pape_results] = pape_message
-      end
     when OpenID::Consumer::SETUP_NEEDED
       flash[:alert] = "Immediate request failed - Setup Needed"
     when OpenID::Consumer::CANCEL
       flash[:alert] = "OpenID transaction cancelled."
-    else
-    end
-    case oidresp.status
     when OpenID::Consumer::SUCCESS
+      flash[:success] = ("Verification of #{oidresp.display_identifier}"\
+                         " succeeded.")
       session[:openid_url] = oidresp.display_identifier
       if getLoginUser
         @uname = getLoginUser.name
@@ -151,25 +108,28 @@ class ConsumerController < ApplicationController
         if getLoginUser.update_last_login
           if params[:keep_login] == "on"
             getLoginUser.update_keep_login(@remote_ip)
-            cookies[:keep_login_token] = { :value => getLoginUser.keep_login_token, :expires => Time.now + 3.days }
+            cookies[:keep_login_token] = {
+              :value => getLoginUser.keep_login_token,
+              :expires => Time.now + 3.days
+            }
           end
-        else
         end
         if params[:fromUrl]
-          redirect_to params[:fromUrl]
+          @redirect_url = params[:fromUrl];
         else
-          redirect_to :controller => 'mypage', :action => 'index'
+          @redirect_url = url_for(:controller => 'mypage', :action => 'index')
         end
       else
         if params[:fromUrl]
-          redirect_to :action => 'signup', :fromUrl => params[:fromUrl]
+          @redirect_url = url_for(:action => 'signup',
+                                  :fromUrl => params[:fromUrl])
         else
-          redirect_to :action => 'signup'
+          @redirect_url = url_for(:action => 'signup')
         end
       end
     else
-      redirect_to :action => 'index'
     end
+    redirect_to(@redirect_url)
   end
 
   def sign_out
@@ -177,54 +137,63 @@ class ConsumerController < ApplicationController
     session[:openid_url] = nil
     flash[:success] = "LogOut Complete."
     if params[:fromUrl]
-      redirect_to :action => 'index', :fromUrl => params[:fromUrl]
+      redirect_to(:action => 'index', :fromUrl => params[:fromUrl])
     else
-      redirect_to :action => 'index'
+      redirect_to(:action => 'index')
     end
   end
 
   def signup
-    if !session[:openid_url]
+    if session[:openid_url]
+      if getLoginUser
+        redirect_to(:controller => 'mypage', :action => 'index')
+      end
+    else
       flash[:error] = "Error: To signup, you have to login by openid."
-      redirect_to :action => 'index'
+      redirect_to(:action => 'index')
     end
   end
 
   def signup_complete
     if session[:openid_url]
-      @creating_user_id = "#{params[:creating_user_id]}";
-      @edit_profile_flg = (params[:edit_profile_flg] == "1")
-      @error_message = User.regist(@creating_user_id, session[:openid_url])
-      if !@error_message
-        if params[:image]
-          getLoginUser.update_image_path(params[:image])
-        end
-        flash[:success] = "Hello " + @creating_user_id + ". SignUp was successfully completed."
-        if @edit_profile_flg
-          redirect_to :controller => 'settings', :action => 'profile_edit'
-        else
-          if params[:fromUrl]
-            redirect_to params[:fromUrl]
-          else
-            redirect_to :controller => 'mypage',:action => 'index'
-          end
-        end
+      if getLoginUser
+        redirect_to(:controller => 'mypage', :action => 'index')
       else
-        flash[:error] = @error_message
-        redirect_to :back
+        @creating_user_id = "#{params[:creating_user_id]}";
+        @edit_profile_flg = (params[:edit_profile_flg] == "1")
+        @error_message = User.regist(@creating_user_id, session[:openid_url])
+        if !@error_message
+          if params[:image]
+            getLoginUser.update_image_path(params[:image])
+          end
+          flash[:success] = "Hello " + @creating_user_id +
+            ". SignUp was successfully completed."
+          if @edit_profile_flg
+            redirect_to(:controller => 'settings', :action => 'profile_edit')
+          else
+            if params[:fromUrl]
+              redirect_to(params[:fromUrl])
+            else
+              redirect_to(:controller => 'mypage',:action => 'index')
+            end
+          end
+        else
+          flash[:error] = @error_message
+          redirect_to(:back)
+        end
       end
     else
       flash[:error] = "Error: To signup, you have to login by openid."
-      redirect_to :action => 'index'
+      redirect_to(:action => 'index')
     end
   end
 
   def get_user_existing
     @uname = params[:creating_user_name]
     if User.exists?(@uname)
-      render :text => "EXISTS"
+      render(:text => "EXISTS")
     else
-      render :text => "NONE"
+      render(:text => "NONE")
     end
   end
 
