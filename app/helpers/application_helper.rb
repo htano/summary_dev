9ab2d37b07dd207fd480d2df3e2d074114dcbaed
htano <tanohiro@gmail.com>
2013-10-05 22:28:24 +0900

    refactor navigation bar  - add profile image and user name which links to mypage  - add setting image which deals with account setting and signing out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 module ApplicationHelper
   def simple_format_2(text)
     return text if text.nil?
-	  text = h text
-	  text.gsub(/\r\n|\r|\n/, "<br/>").html_safe
+    text = h text
+    text.gsub(/\r\n|\r|\n/, "<br/>").html_safe
   end
+
+  def render_user_name
+    if signed_in?
+      return link_to image_tag(get_login_user.prof_image, :size => "23x23") + 
+                      content_tag(:p, get_current_user_name), :controller => "mypage", :action => "index"
+    else
+    end
+  end
+
 end
