2709c17143b16bdbc72db59100481547fd1e2270
toru1055 <toru1055h@gmail.com>
2013-10-05 23:07:41 +0900

    Add small and large listing of hotentry pages.

--- app/controllers/hotentry_controller.rb
+++ app/controllers/hotentry_controller.rb
@@ -6,4 +6,18 @@ class HotentryController < ApplicationController
   def normal
     @entries = Article.get_hotentry_articles
   end
+
+  def small
+    @order = 1
+    @hash = {}
+    @entries = Article.get_hotentry_articles
+    @entries.each do |e|
+      @hash[e.id] = @order
+      @order += 1
+    end
+  end
+
+  def large
+    @entries = Article.get_hotentry_articles
+  end
 end
