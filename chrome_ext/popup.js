da62e5d491f1d7d7fe4e51567a2cead4ea2f3a31
shingo0809 <shingo0809@gmail.com>
2013-10-10 00:01:12 +0900

    2013/10/10 00:01

--- chrome_ext/popup.js
+++ chrome_ext/popup.js
@@ -3,7 +3,6 @@ BLANK = ""
 /*TODO ホストの書き方*/
 $(document).ready( function(){
   var bg = window.chrome.extension.getBackgroundPage();
-  console.log( $("p#p_title").text() );
   $("p#p_title").text(bg.current_tab.title);
   $("p#p_url").text(bg.current_tab.url);
 
@@ -49,13 +48,10 @@ $(document).ready( function(){
             element.id = "recommend_tag_"+(i);
             element.innerHTML = tags[i-1].trim();
             objBody.appendChild(element);
-            //$("#recommend_tag_"+(i+1)).click(clickRecommendTag);
-            //$("#recommend_tag_"+(i+1)).click(clickRecommendTag(tags[i].trim()));
+            $("#recommend_tag_"+(i)).click(function(){clickRecommendTag(this)});
             i++;
           }
           $("#recommend_tag").find("span").addClass("recommend_tag");
-          //$("#recommend_tag").on("click", {class:"recommend_tag"}, clickRecommendTag());
-          //$(".recommend_tag").click(clickRecommendTag("test"));
         }
       }
   });
@@ -70,28 +66,21 @@ $(document).ready( function(){
           var tags = data.split(",");
           var i=1;
           var objBody = document.getElementById("recent_tag");
-          console.log(tags.length);
           while(tags.length>=i){
-            console.log(i);
             var tag = tags[i-1].trim();
             var element = document.createElement("span"); 
             element.id = "recent_tag_"+(i);
             element.innerHTML = tag;
-            //element.onclick = function(){clickRecentTag(tag)};
-            //element.onclick = function(tag){ return clickRecentTag(tag) };
-            //element.onclick = clickRecentTag(tags[i].trim());
-            //element.setAttribute("onclick",clickRecentTag);
             objBody.appendChild(element);
-            $("#recent_tag_"+(i)).click(function(){clickRecentTag(i)});
-            //$(element.id).click(clickRecentTag());
+            $("#recent_tag_"+(i)).click(function(){clickRecentTag(this)});
             i++;
           }
           $("#recent_tag").find("span").addClass("recent_tag");
-          //$(".recent_tag").click(clickRecentTag($(this)));
         }
       }
   });
-
+  
+  //TODO タグ情報を送信してChromeからでもタグ有で登録出来るようにする
   $("#p_button").click(function(){
     $("img.a_load").attr("style", "visibility:visible;");
     $.ajax({
@@ -134,45 +123,77 @@ function setBtnDisabled(){
   $("#p_button").attr("disabled", true);
   $("#p_button").attr("class", "button button-flat.disabled");
 }
-/*
-function clickRecommendTag(){
-  alert("OK");
-}
-*/
-function clickRecentTag(id){
-  //document.getElementById("tag_text_1").value = value;
-  console.log("!!!!!");
-  console.log(id);
-  console.log("!!!!!");
-  value = document.getElementById("recent_tag_" + id).value();
-  tag_class = document.getElementById("recent_tag_" + id).getAttribute("class")
-  if (tag_class == "recent_tag"){
+
+
+function clickRecommendTag(obj){
+  value = obj.innerHTML
+  if (obj.className == "recommend_tag"){
     var text_list = new Array();
     for (var i=1 ; i<=10 ; i++){
-      text_list.push(document.getElementById("tag_text_" + i).value());
+      text_list.push(document.getElementById("tag_text_" + i).value);
     }
 
-    if(text_list.indexOf(value) != -1){
-      document.getElementById("recent_tag_" + id).setAttribute("class", "recent_tag_pushed");
+    if(text_list.indexOf(value) != -1){ 
+      obj.className = "recommend_tag_pushed";
       return;
     }
 
     for (var i=1 ; i<=10 ; i++){
-      if (document.getElementById("tag_text_" + i).value() == BLANK){
-        document.getElementById("tag_text_" + i).value() = value;
-        document.getElementById("recent_tag_" + id).setAttribute("class", "recent_tag_pushed");
+      if (document.getElementById("tag_text_" + i).value == BLANK){
+        document.getElementById("tag_text_" + i).value = value;
+        obj.className = "recommend_tag_pushed";
         return;
       }
-      alert("Please set tags within 10.");
     }
 
   } else {
     for (var i=1 ; i<=10 ; i++){
-      if (document.getElementById("tag_text_" + i).value() == value){
-        document.getElementById("tag_text_" + i).value() = BLANK;
-        document.getElementById("recent_tag_" + id).setAttribute("class", "recent_tag");
+      if (document.getElementById("tag_text_" + i).value == value){
+        document.getElementById("tag_text_" + i).value = BLANK;
+        obj.className = "recommend_tag";
         return;
       }
     }
   }
 }
+
+
+function clickRecentTag(obj){
+  value = obj.innerHTML
+  if (obj.className == "recent_tag"){
+    var text_list = new Array();
+    for (var i=1 ; i<=10 ; i++){
+      text_list.push(document.getElementById("tag_text_" + i).value);
+    }
+
+    if(text_list.indexOf(value) != -1){ 
+      obj.className = "recent_tag_pushed";
+      return;
+    }
+
+    for (var i=1 ; i<=10 ; i++){
+      if (document.getElementById("tag_text_" + i).value == BLANK){
+        document.getElementById("tag_text_" + i).value = value;
+        obj.className = "recent_tag_pushed";
+        return;
+      }
+    }
+
+  } else {
+    for (var i=1 ; i<=10 ; i++){
+      if (document.getElementById("tag_text_" + i).value == value){
+        document.getElementById("tag_text_" + i).value = BLANK;
+        obj.className = "recent_tag";
+        return;
+      }
+    }
+  }
+}
+/*
+$("#test").click(function(){
+  for (var i=1 ; i<=10 ; i++){
+    document.getElementById('tag_text_' + i).value = BLANK
+  }
+  alert("clickDelete()");
+});
+*/
