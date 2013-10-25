b703d33112fa0eb91b47c7d1d955727560116e1e
shingo0809 <shingo0809@gmail.com>
2013-10-25 22:50:17 +0900

    enable private user search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -61,7 +61,7 @@ class SearchController < ApplicationController
     @searchtext = params[:searchtext]
     @target = params[:target] == BLANK || params[:target] == nil ? "1" : params[:target]
     @sort = params[:sort] == BLANK || params[:sort] == nil ? "1" : params[:sort]
-    @users = User.where(["name LIKE ? or full_name LIKE ?", "%"+@searchtext+"%", "%"+@searchtext+"%"]).where("public_flg" => true, "yuko_flg" => true)
+    @users = User.where(["name LIKE ? or full_name LIKE ?", "%"+@searchtext+"%", "%"+@searchtext+"%"]).where("yuko_flg" => true)
     @user_num = @users == BLANK || @users == nil ? 0 : @users.length
 
     case @sort
