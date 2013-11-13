e2f2dffce9cfb870965f423c4a4f72ebc267fcd1
htano <tanohiro@gmail.com>
2013-11-13 23:35:21 +0900

    add script generates test data

---
+++ scripts/generate_fixtures/favorite_users/generate_favorite_users_yml.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+num=0
+main_user_id=1
+
+output_data() {
+  echo "favorite_user_data$1:"
+  echo "    user_id: $2"
+  echo "    favorite_user_id: $3"
+}
+
+while [ $num -lt 30 ]
+do
+  num=`expr ${num} + 1`
+  id=`expr ${num} + 1`
+  output_data ${num} ${id} ${main_user_id}
+done
+
+id=1
+while [ $num -lt 60 ]
+do
+  num=`expr ${num} + 1`
+  id=`expr ${id} + 1`
+  output_data ${num} ${main_user_id} ${id}
+done
---
+++ scripts/generate_fixtures/users/README.md
@@ -0,0 +1,1 @@
+Usage : ./generate_user_yml.sh user_data > user.yml  
