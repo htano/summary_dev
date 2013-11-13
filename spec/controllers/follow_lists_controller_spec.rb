e8d3436b594608227f447f3b703b731d4c91c154
htano <tanohiro@gmail.com>
2013-11-13 23:33:42 +0900

    add GET followings test

--- spec/controllers/follow_lists_controller_spec.rb
+++ spec/controllers/follow_lists_controller_spec.rb
@@ -27,15 +27,10 @@ describe FollowListsController do
         })
 =end
       session[:openid_url] = "oauth://twitter/12345"
-      follower_uid = 
-        FavoriteUser.where(:favorite_user_id => 1).limit(DISPLAY_USER_NUM).pluck(:user_id)
-      @summary_followers_with_no_number = 
-        User.where(:id => follower_uid)
-
-      follower_uid = 
-        FavoriteUser.where(:favorite_user_id => 1).limit(DISPLAY_USER_NUM).offset(DISPLAY_USER_NUM).pluck(:user_id)
-      @summary_followers_with_number = 
-        User.where(:id => follower_uid)
+      @first_display_followers = 
+        [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+      @second_display_followers = 
+        [22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
     end
 
     it "access to route successfully" do
@@ -48,11 +43,13 @@ describe FollowListsController do
     end
     it "get followers with no number" do
       get :followers
-      expect(assigns[:followers]).to eq(@summary_followers_with_no_number)
+      ids = assigns[:followers].pluck(:id)
+      expect(ids).to match_array(@first_display_followers)
     end
     it "get followers with number" do
       get :followers, :number => 1
-      expect(assigns[:followers]).to eq(@summary_followers_with_number)
+      ids = assigns[:followers].pluck(:id)
+      expect(ids).to match_array(@second_display_followers)
     end
     it "get followers with invalid number" do
       get :followers, :number => 2
@@ -60,8 +57,75 @@ describe FollowListsController do
     end
     it "get followers with minus number" do
       get :followers, :number => -1
-      expect(assigns[:followers]).to eq(@summary_followers_with_no_number)
+      ids = assigns[:followers].pluck(:id)
+      expect(ids).to match_array(@first_display_followers)
     end
   end
+
+  describe "GET #following without signing in" do
+    it "access to route successfully" do
+      get :following, :name => "foo"
+      expect(response).to be_success
+    end
+    it "access to route without param" do
+      get :following
+      expect(response).to redirect_to :controller => 'consumer', 
+                                      :action => 'index'
+    end
+  end
+
+  describe "GET #following with signing in" do
+    before(:each) do
+      session[:openid_url] = "oauth://twitter/12345"
+      @first_display_followings = 
+        [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+      @second_display_followings = 
+        [22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    end
+    it "access to route successfully" do
+      get :following
+      expect(response).to be_success
+    end
+    it "get user name successfully" do
+      get :following
+      expect(assigns[:user_name]).to eq("summary")
+    end
+    it "get following with no number" do
+      get :following
+      ids = assigns[:following_users].pluck(:id)
+      expect(ids).to match_array(@first_display_followings)
+    end
+    it "get following with number" do
+      get :following, :number => 1
+      ids = assigns[:following_users].pluck(:id)
+      expect(ids).to match_array(@second_display_followings)
+    end
+    it "get following with invalid number" do
+      get :following, :number => 2
+      expect(assigns[:following_users]).to eq([])
+    end
+    it "get following with minus number" do
+      get :following, :number => -1
+      ids = assigns[:following_users].pluck(:id)
+      expect(ids).to match_array(@first_display_followings)
+    end
+  end
+
+  describe "GET #suggestion without signing in" do
+    it "access to route successfully" do
+      get :suggestion, :name => "foo"
+      expect(response).to be_success
+    end
+    it "access to route successfully" do
+      get :suggestion, :name => "foo"
+      expect(@candidate_users).to be_false
+    end
+    it "access to route without param" do
+      get :suggestion
+      expect(response).to redirect_to :controller => 'consumer', 
+                                      :action => 'index'
+    end
+  end
+
 
 end
