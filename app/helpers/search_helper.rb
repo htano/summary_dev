178470eed8fa5601b7812fb0b341cd0438d23049
shingo0809 <shingo0809@gmail.com>
2013-11-13 23:50:36 +0900

    update search view

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -15,11 +15,11 @@ module SearchHelper
 
   def render_read_status(article)
     if article.user_articles.where(:user_id =>get_login_user.id).first
-      return button_to "not read",
+      return button_to "Read cancel",
             {:action => "not_read", :controller => "search", :article_id => article.id},
             {:class => "read_it_cancel", :remote => true}
     else
-      return button_to "read",
+      return button_to "Read later",
             {:action => "read", :controller => "search", :article_id => article.id},
             {:class => "read_it_later", :remote => true}
     end
