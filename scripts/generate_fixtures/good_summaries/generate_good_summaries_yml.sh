a2868c51cba33e7768e8f5765664b3e2f008d668
htano <tanohiro@gmail.com>
2013-11-21 17:23:43 +0900

    - add good summaries test data. - add script genereates good summaries test data.

---
+++ scripts/generate_fixtures/good_summaries/generate_good_summaries_yml.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+output_data() {
+    echo "good_summaries$1:"
+    echo "    user_id: $2"
+    echo "    summary_id : $3"
+}
+
+id=0
+for j in `seq 0 5`
+do
+    for i in `seq 0 10`
+    do
+	id=`expr $id + 1`
+	user_id=`expr $j + 1`
+	summary_id=`expr $i + 1`
+	output_data $id $user_id $summary_id
+    done
+done
+
