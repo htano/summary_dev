8d0bfd3fb8d51bddcc8a9ebb5b4b47965749f843
htano <tanohiro@gmail.com>
2013-11-21 20:12:14 +0900

    fix to make value unique

--- scripts/generate_fixtures/user_article_tags/convert_tags.rb
+++ scripts/generate_fixtures/user_article_tags/convert_tags.rb
@@ -7,9 +7,15 @@ id = 0;
 csv.each do |row|
   id = id + 1
   user_article_id = row["user_article_id"].to_i
-  while user_article_id > 130 do
-    user_article_id = user_article_id / 2
+  if user_article_id >= 300
+    user_article_id = user_article_id - 299
+  elsif user_article_id >= 200 && user_article_id < 300
+    user_article_id = user_article_id - 199
+  elsif user_article_id >= 100 && user_article_id < 200
+    user_article_id = user_article_id - 99
+  else
   end
+
   hash["tags_#{id}"] = {
     "id" => id,
     "user_article_id" => user_article_id,
