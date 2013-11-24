841dbde397848bd5cee964dc54fef167da0e1ca6
tetsuma-y <usdeurjpy0305@gmail.com>
2013-11-24 12:46:03 +0900

    Enhance development log for us

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ SummaryDev::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  config.log_formatter = ::Logger::Formatter.new
 end
