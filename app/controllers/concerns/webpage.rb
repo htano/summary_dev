909e4b9e3b40f6a211e76a1adaeb35ab5ea722aa
toru1055 <toru1055h@gmail.com>
2013-11-24 22:52:09 +0900

    Enable delayed_job in heroku(#117)

--- app/controllers/concerns/webpage.rb
+++ app/controllers/concerns/webpage.rb
@@ -36,13 +36,13 @@ module Webpage
       if article
         summarize_job = SummarizingJob.new(article.id)
         summarize_job.delay.run
-        #summarize_job.run
         classify_job = ClassifyingJob.new(article.id)
         classify_job.delay.run
-        #classify_job.run
         cluster_job = ClusteringJob.new(article.id)
         cluster_job.delay.run
-        #cluster_job.run
+        fork do
+          exec(Rails.root.to_s + "/bin/delayed_job run --exit-on-complete")
+        end
       end
     end
     get_login_user.add_cluster_id(article.cluster_id)
