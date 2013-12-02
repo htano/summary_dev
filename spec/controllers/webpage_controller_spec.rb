ff69e7fbeb887c146abbee5ffc0e877be76884ea
shingo0809 <shingo0809@gmail.com>
2013-12-03 00:33:07 +0900

    update webpage_controller test

--- spec/controllers/webpage_controller_spec.rb
+++ spec/controllers/webpage_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe WebpageController do
-  #fixtures(:all)
+  fixtures(:all)
 
   describe "GET #add without signing in" do
     it "access to route without no parameters" do
@@ -36,9 +36,16 @@ describe WebpageController do
     before(:each) do
       session[:openid_url] = "oauth://twitter/12345"
     end
-    it "access to route without no parameters" do
-      post :add_confirm, :name => "http://www.yahoo.co.jp/", :add_flag => "true"
-      p @title
+    it "add_confirm with unknown article" do
+      post :add_confirm, :url => "http://www.yahoo.co.jp/", :add_flag => "true"
+      response.should be_success
+      assigns[:title].should == "Yahoo! JAPAN"
+      assigns[:top_rated_tags].should == nil
+      assigns[:recent_tags].should eq ["ge", "MVC", "マーケティング", "Rails", "sqlite3", "Ruby"]
+      assigns[:set_tags].should eq []
+      assigns[:summary_num].should == nil
+      assigns[:reader_num] == nil
+      assigns[:article_id] == nil
     end
   end
 end
