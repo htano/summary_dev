fe523b08e1997e5bb02488ee54afc20ba095f210
shingo0809 <shingo0809@gmail.com>
2013-12-04 00:09:58 +0900

    update webpage controller test

--- spec/controllers/webpage_controller_spec.rb
+++ spec/controllers/webpage_controller_spec.rb
@@ -34,18 +34,40 @@ describe WebpageController do
 
   describe "POST #add_confirm with signing in" do
     before(:each) do
-      session[:openid_url] = "oauth://twitter/12345"
+      session[:openid_url] = "oauth://facebook/12354"
     end
     it "add_confirm with unknown article" do
       post :add_confirm, :url => "http://www.yahoo.co.jp/", :add_flag => "true"
       response.should be_success
       assigns[:title].should == "Yahoo! JAPAN"
       assigns[:top_rated_tags].should == nil
-      assigns[:recent_tags].should eq ["ge", "MVC", "マーケティング", "Rails", "sqlite3", "Ruby"]
-      assigns[:set_tags].should eq []
+      assigns[:recent_tags].should ==  ["１２３４５６７８９０", "１２３４５６７８９５", "１２３４５６７８９４", "１２３４５６７８９３", "１２３４５６７８９２", "１２３４５６７８９１", "Rails", "SQL", "Ruby", "ruby"]
+      assigns[:set_tags].should == []
       assigns[:summary_num].should == nil
       assigns[:reader_num] == nil
       assigns[:article_id] == nil
+    end
+    it "add_confirm with known article no set tag" do
+      post :add_confirm, :url => "http://zasshi.news.yahoo.co.jp/article?a=20130930-00010000-bjournal-bus_all", :add_flag => "true"
+      response.should be_success
+      assigns[:title].should == "auのKDDI、あきれた二枚舌営業〜購入時に虚偽説明、強いクレームには特別に補償対応 （Business Journal） - Yahoo!ニュース"
+      assigns[:top_rated_tags].should == []
+      assigns[:recent_tags].should ==  ["１２３４５６７８９０", "１２３４５６７８９５", "１２３４５６７８９４", "１２３４５６７８９３", "１２３４５６７８９２", "１２３４５６７８９１", "Rails", "SQL", "Ruby", "ruby"]
+      assigns[:set_tags].should == []
+      assigns[:summary_num].should == 0
+      assigns[:reader_num] == 0
+      assigns[:article_id] == "1"
+    end
+    it "add_confirm with known article set tag" do
+      post :add_confirm, :url => "http://qiita.com/toyama0919/items/3e165e41232266edbb23", :add_flag => "true"
+      response.should be_success
+      assigns[:title].should == "rubyのgeocoderの使い方 - Qiita [キータ]"
+      assigns[:top_rated_tags].should == ["ge"]
+      assigns[:recent_tags].should ==  ["１２３４５６７８９０", "１２３４５６７８９５", "１２３４５６７８９４", "１２３４５６７８９３", "１２３４５６７８９２", "１２３４５６７８９１", "Rails", "SQL", "Ruby", "ruby"]
+      assigns[:set_tags].should == ["ge"]
+      assigns[:summary_num].should == 3
+      assigns[:reader_num].should == 1
+      assigns[:article_id].should == 44
     end
   end
 end
