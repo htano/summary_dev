b1d76ec726c79671bfdac399957328bbd5be29fa
toru1055 <toru1055h@gmail.com>
2013-12-15 09:03:19 +0900

    Update article classifier model

--- lib/contents-extractor/base-extractor.rb
+++ lib/contents-extractor/base-extractor.rb
@@ -84,11 +84,15 @@ class ContentsExtractor::BaseExtractor
       p.gsub!(/([\u300C][^\u300D]+[\u300D])/){
         $1.gsub(/[。．]/, "") 
       }
+      p.gsub!(/([\u0028][^\u0029]+[\u0029])/){
+        $1.gsub(/[。．]/, "") 
+      }
       if p.length > 0
         next if p =~ /^\d+.*\d{4}.\d{2}.\d{2}.*ID.*$/;
         p_obj = ContentsExtractor::Paragraph.new
         p.split(/[。．]/).each do |s|
           s.gsub!(/[\u3010][^\u3011]+[\u3011]/,"")
+          s.gsub!(/[\u0028][^\u0029]+[\u0029]/,"")
           next if s =~ / \- GIGAZINE$/;
           next if s =~ /^\d+.*\d{4}.\d{2}.\d{2}.*ID.*$/;
           next if s =~ /^[\x20-\x7E]+$/;
