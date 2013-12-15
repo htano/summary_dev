d0e8ea61d3dc0f00d3228fd94e1674b0b9792fbc
toru1055 <toru1055h@gmail.com>
2013-12-15 15:55:00 +0900

    merged

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131203143214) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: true do |t|
     t.string   "url"
     t.string   "title"
