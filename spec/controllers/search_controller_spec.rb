76354b01d3c177e6ba401917ec67d9cc21f83ade
shingo0809 <shingo0809@gmail.com>
2013-12-15 13:27:02 +0900

    update search test

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -13,32 +13,122 @@ describe SearchController do
   end
 
   describe "GET #search_article" do
+    it "search_article searchtext nil" do
+      get :search_article
+      assigns[:searchtext].should == nil
+      assigns[:target].should == "1"
+      assigns[:type].should == "1"
+      assigns[:sort].should == "1"
+      assigns[:category].should == "0"
+      assigns[:articles].should == nil
+      assigns[:article_num].should == 0
+      assigns[:type_text].should == "タイトル＆本文"
+      assigns[:sort_menu_title].should == nil
+      expect(response).to redirect_to :controller => 'search',:action => 'index'
+    end
     it "search_article type 1 sort 1" do
       get :search_article, :searchtext => "", :type => "1" , :sort => "1"
+=begin
+      assigns[:searchtext].should == nil
+      assigns[:target].should == "1"
+      assigns[:type].should == "1"
+      assigns[:sort].should == "1"
+      assigns[:category].should == "0"
+      assigns[:articles].should == nil
+      assigns[:article_num].should == 0
+      assigns[:type_text].should == "タイトル＆本文"
+      assigns[:sort_menu_title].should == nil
+=end
       expect(response).to redirect_to :controller => 'search',:action => 'index'
     end
     it "search_article type 2 sort 1" do
       get :search_article, :searchtext => "", :type => "2" , :sort => "1"
+=begin
+      assigns[:searchtext].should == nil
+      assigns[:target].should == "1"
+      assigns[:type].should == "1"
+      assigns[:sort].should == "1"
+      assigns[:category].should == "0"
+      assigns[:articles].should == nil
+      assigns[:article_num].should == 0
+      assigns[:type_text].should == "タイトル＆本文"
+      assigns[:sort_menu_title].should == nil
+=end
       expect(response).to redirect_to :controller => 'search',:action => 'index'
     end
     it "search_article type 3 sort 1" do
       get :search_article, :searchtext => "", :type => "3" , :sort => "1"
+=begin
+      assigns[:searchtext].should == nil
+      assigns[:target].should == "1"
+      assigns[:type].should == "1"
+      assigns[:sort].should == "1"
+      assigns[:category].should == "0"
+      assigns[:articles].should == nil
+      assigns[:article_num].should == 0
+      assigns[:type_text].should == "タイトル＆本文"
+      assigns[:sort_menu_title].should == nil
+=end
       expect(response).to redirect_to :controller => 'search',:action => 'index'
     end
     it "search_article type 4 sort 1" do
       get :search_article, :searchtext => "", :type => "4" , :sort => "1"
+=begin
+      assigns[:searchtext].should == nil
+      assigns[:target].should == "1"
+      assigns[:type].should == "1"
+      assigns[:sort].should == "1"
+      assigns[:category].should == "0"
+      assigns[:articles].should == nil
+      assigns[:article_num].should == 0
+      assigns[:type_text].should == "タイトル＆本文"
+      assigns[:sort_menu_title].should == nil
+=end
       expect(response).to redirect_to :controller => 'search',:action => 'index'
     end
     it "search_article type 1 sort 2" do
       get :search_article, :searchtext => "", :type => "1" , :sort => "2"
+=begin
+      assigns[:searchtext].should == nil
+      assigns[:target].should == "1"
+      assigns[:type].should == "1"
+      assigns[:sort].should == "1"
+      assigns[:category].should == "0"
+      assigns[:articles].should == nil
+      assigns[:article_num].should == 0
+      assigns[:type_text].should == "タイトル＆本文"
+      assigns[:sort_menu_title].should == nil
+=end
       expect(response).to redirect_to :controller => 'search',:action => 'index'
     end
     it "search_article type 1 sort 3" do
       get :search_article, :searchtext => "", :type => "1" , :sort => "3"
+=begin
+      assigns[:searchtext].should == nil
+      assigns[:target].should == "1"
+      assigns[:type].should == "1"
+      assigns[:sort].should == "1"
+      assigns[:category].should == "0"
+      assigns[:articles].should == nil
+      assigns[:article_num].should == 0
+      assigns[:type_text].should == "タイトル＆本文"
+      assigns[:sort_menu_title].should == nil
+=end
       expect(response).to redirect_to :controller => 'search',:action => 'index'
     end
     it "search_article type 1 sort 4" do
       get :search_article, :searchtext => "", :type => "1" , :sort => "4"
+=begin
+      assigns[:searchtext].should == nil
+      assigns[:target].should == "1"
+      assigns[:type].should == "1"
+      assigns[:sort].should == "1"
+      assigns[:category].should == "0"
+      assigns[:articles].should == nil
+      assigns[:article_num].should == 0
+      assigns[:type_text].should == "タイトル＆本文"
+      assigns[:sort_menu_title].should == nil
+=end
       expect(response).to redirect_to :controller => 'search',:action => 'index'
     end
   end
