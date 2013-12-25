6c1956da9cdd98ed0401319b9215176e603d80f1
toru1055 <toru1055h@gmail.com>
2013-12-25 21:34:28 +0900

    Reduce some redirects to speedup hotentry page(#193)

--- app/controllers/hotentry_controller.rb
+++ app/controllers/hotentry_controller.rb
@@ -2,18 +2,26 @@ class HotentryController < ApplicationController
   def index
     case cookies[:hotentry_view_type]
     when 'small'
-      redirect_to(:action => 'small', 
-                  :category => params[:category])
+      small
+      @view_type = 'small'
+      render :template => 'hotentry/small'
     when 'large'
-      redirect_to(:action => 'large',
-                  :category => params[:category])
+      large
+      @view_type = 'large'
+      render :template => 'hotentry/large'
     else
-      redirect_to(:action => 'normal',
-                  :category => params[:category])
+      normal
+      @view_type = 'normal'
+      render :template => 'hotentry/normal'
     end
   end
 
   def normal
+    unless action_name == 'index'
+      cookies[:hotentry_view_type] = { :value => 'normal' }
+      redirect_to(:action => 'index',
+                  :category => params[:category])
+    end
     if params[:category]
       @category_name = params[:category]
     else
@@ -31,6 +39,11 @@ class HotentryController < ApplicationController
   end
 
   def small
+    unless action_name == 'index'
+      cookies[:hotentry_view_type] = { :value => 'small' }
+      redirect_to(:action => 'index',
+                  :category => params[:category])
+    end
     if params[:category]
       @category_name = params[:category]
     else
@@ -54,6 +67,11 @@ class HotentryController < ApplicationController
   end
 
   def large
+    unless action_name == 'index'
+      cookies[:hotentry_view_type] = { :value => 'large' }
+      redirect_to(:action => 'index',
+                  :category => params[:category])
+    end
     if params[:category]
       @category_name = params[:category]
     else
