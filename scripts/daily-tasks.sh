28f8d2ef555683f0e1b29cf5496c4ec66c279557
toru1055 <toru1055h@gmail.com>
2014-01-04 22:39:04 +0900

    Update daily task script

--- scripts/daily-tasks.sh
+++ scripts/daily-tasks.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 RAILS_ROOT=$1
-R_ENV=$2
+BINDIR=$2
+R_ENV=$3
 cd ${RAILS_ROOT}
 
 # stop delayed_job
-RAILS_ENV=${R_ENV} bin/delayed_job stop
+RAILS_ENV=${R_ENV} ${BINDIR}/bundle exec bin/delayed_job stop
 
 # make common daily data
-bin/rails runner scripts/daily-tasks/common/mklist-class-title-url.rb
+${BINDIR}/rails runner scripts/daily-tasks/common/mklist-class-title-url.rb
 
 # make document frequency
-bin/rails runner scripts/daily-tasks/text-analyzer/mkdf.rb
+${BINDIR}/rails runner scripts/daily-tasks/text-analyzer/mkdf.rb
 
 # add common daily data with tfidf
-bin/rails runner scripts/daily-tasks/common/mklist-class-title-url-tfidf.rb
+${BINDIR}/rails runner scripts/daily-tasks/common/mklist-class-title-url-tfidf.rb
 
 # start delayed_job
-RAILS_ENV=${R_ENV} bin/delayed_job start
+RAILS_ENV=${R_ENV} ${BINDIR}/bundle exec bin/delayed_job start
