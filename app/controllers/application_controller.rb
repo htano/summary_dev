949e1971d8815c272ec79c7058a91197f67641c6
toru1055 <toru1055h@gmail.com>
2014-02-09 16:24:19 +0900

    enable rescue exception in test env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  if Rails.env.production?
+  unless Rails.env.development?
     rescue_from Exception do
       render :template => "errors/error_500", :status => 500, :layout => 'application'
     end
