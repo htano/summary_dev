4257934cb14bdc489a2bd677a80ab53359835f2a
shingo0809 <shingo0809@gmail.com>
2014-02-10 20:21:45 +0900

    separate navbar view&javascript

---
+++ app/assets/javascripts/_navbar.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  var signed_in_status = $.cookie("signed_in_status");
+  if(signed_in_status == "true"){
+    set_type_nav();
+  } else {
+    $("#navbar_form").attr("action","/search/search_article");
+    $("#search_nav").val("検索");
+    /*$("#search_nav").val(I18n.t('search.search'));*/
+    /*$("#searchtext_nav").attr("placeholder",I18n.t('placeholder.search_text'));*/
+    $("#searchtext_nav").attr("placeholder","気になる言葉に関連する記事を検索しましょう！");
+  }
+});
+
+function checkSearchtext(){
+  if($('#searchtext_nav').val() == ""){
+    return false;
+  }
+}
+
+function click_dropdown_manu_search(){
+  $("#navbar_form").attr("action","/search/search_article");
+  $("#navbar_form").attr("method","GET");
+  $("#search_nav").val("検索");
+  /*$("#search_nav").val(I18n.t('search.search'));*/
+  /*$("#searchtext_nav").attr("placeholder",I18n.t('placeholder.search_text'));*/
+  $("#searchtext_nav").attr("placeholder","気になる言葉に関連する記事を検索しましょう！");
+  $.cookie('type_nav', 1, {path: '/'});
+}
+
+function click_dropdown_manu_add(){
+  $("#navbar_form").attr("action","/webpage/add_confirm");
+  $("#navbar_form").attr("method","POST");
+  $("#navbar_form").attr("add_flag","true");
+  $("#search_nav").val("登録");
+  /*$("#search_nav").val(I18n.t('webpage.add'));*/
+  /*$("#searchtext_nav").attr("placeholder",I18n.t('placeholder.input_url_text'));*/
+  $("#searchtext_nav").attr("placeholder","URLを入力して記事を登録しましょう！");
+  $.cookie('type_nav', 2, {path: '/'});
+}
+
+function set_type_nav(){
+  var type_nav = $.cookie("type_nav");
+  if(type_nav == "1") {
+    click_dropdown_manu_search();
+  } else {
+    click_dropdown_manu_add();
+  }
+}
