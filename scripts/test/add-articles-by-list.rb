2096a4bbc10a947f73bf6aec45c93dd658b97faf
toru1055 <toru1055h@gmail.com>
2014-02-16 12:06:50 +0900

    add domain max counter for news ranking

--- scripts/test/add-articles-by-list.rb
+++ scripts/test/add-articles-by-list.rb
@@ -1,17 +1,22 @@
 # coding: utf-8
 require 'webpage'
 require 'nokogiri'
+require 'uri'
 include Webpage
 ADD_PAGE_NUM = 10
+MAX_DOMAIN_COUNTER = 2
 URL_SCORE = "#{Rails.root}/tmp/url_score_uniq.txt"
 max_score = 0.01
 count = 0
+domain_counter = Hash.new(0)
 open(URL_SCORE) do |file|
   file.each do |line|
     line.chomp!
     url, score = line.split("\s")
     score = score.to_f
+    domain = URI(url).host
     max_score = score if(score > max_score)
+    next if domain_counter[domain] >= MAX_DOMAIN_COUNTER
     next if url =~ %r{^http://sp\.m\.reuters\.co\.jp/}
     next if url =~ %r{^http://rd\.yahoo\.co\.jp}
     next if Article.find_by_url(url)
@@ -32,6 +37,7 @@ open(URL_SCORE) do |file|
       end
     end
     count += 1
+    domain_counter[domain] += 1
     Rails.logger.info("[#{count}] #{url}, #{score}")
     break if count > ADD_PAGE_NUM
     article = add_webpage(url)
