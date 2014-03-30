74c9ec310b16620f68270db8623c9dd55717ed22
toru1055 <toru1055h@gmail.com>
2014-03-31 07:46:31 +0900

    update collect news script

--- scripts/test/collect_news_tweet.sh
+++ scripts/test/collect_news_tweet.sh
@@ -4,4 +4,4 @@ BINDIR=$2
 R_ENV=$3
 cd ${RAILS_ROOT}
 
-${BINDIR}/rails runner scripts/test/test_tweetstream.rb > tmp/user_url.txt -e ${R_ENV}
+${BINDIR}/rails runner scripts/test/test_tweetstream_from_list.rb > tmp/user_url.txt -e ${R_ENV}
